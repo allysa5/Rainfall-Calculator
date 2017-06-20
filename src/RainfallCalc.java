29e2b0a8a46b46dd6300ffa708fbc85c50139359
Allysa5 <allysa_adams@yahoo.com>
2017-06-20 13:17:55 -0400

    Rainfall Calculator

---
+++ src/RainfallCalc.java
@@ -0,0 +1,39 @@
+
+public class RainfallCalc{ 
+
+	public static void main(Double[] args) {
+		// TODO Auto-generated method stub
+		
+		  System.out.print("Rain, rain, go away");
+		  	double length, width, height;
+		
+			if (length > 0 && length > 300){
+				// width length is between 0 and 299
+	
+			}
+			    
+			else if (width > 300 && width < 600){
+				// the length is between 301 and 599
+
+				if (width > 0 && width > 300){
+					//width is between 0 and 299
+		
+				}
+				    
+			else if (width > 300 && width < 600){
+					// the height is between 301 and 599
+							}
+			else if (height > 300 && height < 600){
+					// the height is between 301 and 599
+
+				if (height > 0 && height > 300){
+					//height is between 0 and 299
+			
+					}
+					    
+			else if (height > 300 && height < 600){
+					// the height is between 301 and 599
+					}
+			}
+	
+	}
